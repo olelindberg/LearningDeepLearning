d7c6f362e5ba5d5a3dcd315d51deacd863c059a3
Ole Lindberg <lindberg.ole@gmail.com>
2023-10-30 22:33:59 +0100

    adding stuff

---
+++ linearregression/linearregression.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
+
+class LinearRegressionModel(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LinearRegressionModel, self).__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+
+num_iter = 1000
+
+x = np.array([[1], [2], [3], [3.5]])
+y = np.array([[2], [4], [6], [7.5]])
+
+
+# instantiate model class
+input_dim = 1
+output_dim = 1
+model = LinearRegressionModel(input_dim, output_dim)
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1.0)
+
+for iter in range(num_iter):
+    inputs = Variable(torch.from_numpy(x))
+    labels = Variable(torch.from_numpy(y))  # clear gradients wrt parameters
+
+    outputs = model(inputs.float())
+
+    loss = criterion(outputs.float(), labels.float())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    print("iter {}, loss {}".format(iter, loss.data))
+
+
+predicted = model.linear(Variable(torch.from_numpy(x)).float())
+
+# plot graph
+import matplotlib.pyplot as plt
+
+# clear figure
+plt.clf()  # get predictions
+predicted = predicted  # plot true data
+plt.plot(x, y, "go", label="True data", alpha=0.50)  # plot predictions
+plt.plot(x, predicted.detach().numpy(), "-", label="Predictions", alpha=0.50)  # Legend and plot
+plt.legend(loc="best")
+plt.show()
